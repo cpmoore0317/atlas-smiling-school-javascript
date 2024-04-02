ae45b16938e9e234effbc0347f3361bff56fcfea
cpmoore0317 <kingpkayy@gmail.com>
2024-04-02 10:36:05 -0500

    Added explanitory comments in function for appending new course element

--- scripts.js
+++ scripts.js
@@ -69,6 +69,7 @@ $(document).ready(function () {
     stars,
     duration
   ) {
+    // Appending the HTML structure for the course element to the matchingCourses div
     $("#matchingCourses").append(`
             <div class="col-12 col-sm-6 col-md-4 col-lg-3 mt-4">
                 <div class="card border-0">
